87e15aa99fc435ba8e82291dfc5204af9813233a
penny <925580783@qq.com>
2020-08-26 14:17:05 +0800

    [poker_hands][penny,james]feature:implement should_return_Straight_flush_when_judge_type_given_3H4H5H6H7H test

--- src/main/java/com/example/Judge.java
+++ src/main/java/com/example/Judge.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Judge {
-    public CardType judgeType(String card) {
-        return null;
+    public CardType judgeType(String cards) {
+        String[] cardsArray = cards.split(" ");
+        Arrays.sort(cardsArray);
+        char previousColor = cardsArray[0].charAt(1);
+        char previousPoint = cardsArray[0].charAt(0);
+        for (int i = 1; i < 5; i++) {
+            if (cardsArray[i].charAt(0) != previousPoint + 1 || cardsArray[i].charAt(1) != previousColor) return null;
+            else {
+                previousPoint = cardsArray[i].charAt(0);
+            }
+        }
+        return CardType.STRAIGHT_FLUSH;
     }
 }
