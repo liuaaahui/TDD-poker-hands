4794b5473bf48ae6c5c403ab26d729bc7b58e8f0
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-30 22:49:41 +0800

    [PokerHands][James,Penny]feature: implement test should_return_white_wins_with_high_card_Ace_when_judge_face_given_9HTCAD4H7H_and_KHQC2D8C9S

--- src/main/java/com/example/PokerHands.java
+++ src/main/java/com/example/PokerHands.java
@@ -5,14 +5,40 @@ import java.util.Comparator;
 import java.util.Map;
 
 public class PokerHands {
-    public PokerHands() {
+    private final Generator generator;
+    private final Judge judge;
 
+    public PokerHands() {
+        generator = new Generator();
+        judge = new Judge();
     }
 
     public String play(String white, String black) {
-
-        return null;
+        String[] whiteCards = sortCards(white);
+        String[] blackCards = sortCards(black);
+        return getResult(whiteCards, blackCards);
     }
+
+    private String getCardResult(String[] cards, CardType cardType) {
+        return generator.generateResult(cards, cardType);
+    }
+
+    private String getResult(String[] whiteCards, String[] blackCards) {
+        String winner = "";
+        String cardResult = "";
+        CardType whiteCardType = judge.judgeType(whiteCards);
+        CardType blackCardType = judge.judgeType(blackCards);
+        if (whiteCardType.getWeight() > blackCardType.getWeight()) {
+            winner = "white wins.";
+            cardResult = getCardResult(whiteCards, whiteCardType);
+        } else if (whiteCardType.getWeight() == blackCardType.getWeight()) {
+            if (judge.judgeFaceWithSameType(whiteCards, blackCards, whiteCardType) > 0)
+                winner = "white wins.";
+            cardResult = getCardResult(whiteCards, whiteCardType);
+        }
+        return String.format("%s - with %s", winner,cardResult);
+    }
+
     private String[] sortCards(String cardString) {
         Map<Character, Integer> pointMap = new PointMapper().getMap();
         String[] cards = cardString.split(" ");
