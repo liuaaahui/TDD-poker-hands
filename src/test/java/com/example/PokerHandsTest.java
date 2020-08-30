dcf405d1d88b6273a056d06489e7129d5de1dde9
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-30 22:50:43 +0800

    [PokerHands][James,Penny]feature: add test should_return_black_wins_with_high_card_Ace_when_judge_face_given_and_KHQC2D8C9S_9HTCAD4H7H

--- src/test/java/com/example/PokerHandsTest.java
+++ src/test/java/com/example/PokerHandsTest.java
@@ -3,7 +3,6 @@ package com.example;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.example.CardType.HIGH_CARD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PokerHandsTest {
@@ -25,5 +24,18 @@ public class PokerHandsTest {
 
         //then
         assertEquals("white wins. - with high card: Ace", result);
+    }
+
+    @Test
+    void should_return_black_wins_with_high_card_Ace_when_judge_face_given_and_KHQC2D8C9S_9HTCAD4H7H() {
+        //given
+        String black = "9H TC AD 4H 7H";
+        String white = "KH QC 2D 8C 9S";
+
+        //when
+        String result = pokerHands.play(white, black);
+
+        //then
+        assertEquals("black wins. - with high card: Ace", result);
     }
 }
